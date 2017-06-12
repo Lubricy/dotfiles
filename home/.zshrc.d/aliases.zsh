595984e6074d7bfe8663e9a17095c98654ea70e2
Lubricy Fibber <lubricy@gmail.com>
2017-06-12 17:02:32 +0800

    improve compatibility

--- home/.zshrc.d/aliases.zsh
+++ home/.zshrc.d/aliases.zsh
@@ -1,9 +1,15 @@
-alias ls="ls -G -F"
+if [[ $(uname) =~ Darwin ]]; then
+  alias ls="ls -G -F"
+else
+  alias ls="ls --color -F"
+fi
 foo_cd () {
   cd "$@";
   ls
 }
 alias cd=foo_cd
-alias vim=nvim
-alias vi=nvim
+if command -v nvim >/dev/null 2>&1; then
+  alias vim=nvim
+  alias vi=nvim
+fi
 
