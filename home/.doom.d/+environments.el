aeb38083da53aeae314c804cce244e00c28dc98e
Fu, Luke <hao.fu@fmr.com>
2018-03-06 14:30:12 +0800

    remove confidential info

--- home/.doom.d/+environments.el
+++
@@ -1,5 +0,0 @@
-;;; private/lubricy/+environments.el -*- lexical-binding: t; -*-
-
-(after! tramp
-  (add-to-list 'tramp-remote-process-environment "http_proxy=http://proxy-dln-1.fmr.com:8000")
-  (add-to-list 'tramp-remote-process-environment "https_proxy=http://proxy-dln-1.fmr.com:8000"))
