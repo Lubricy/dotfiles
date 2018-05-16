8e1949911542297a425026263aac3b1bef5dacd2
Fu, Luke <a578203@fmr.com>
2018-05-16 15:57:40 +0800

    update

--- home/.doom.d/config.el
+++ home/.doom.d/config.el
@@ -12,3 +12,15 @@
   (setq company-idle-delay 0.1
         company-minimum-prefix-length 2))
 
+(defun project-set-venv ()
+  "Set python venv to .venv."
+  (let ((venv-path (concat (projectile-project-root) ".venv")))
+       (message "Changing to `%s'" venv-path)
+       (if (file-directory-p venv-path)
+           (pythonic-activate venv-path))))
+
+(after! projectile
+  (add-hook 'projectile-after-switch-project-hook 'project-set-venv))
+
+(provide 'config)
+;;; config.el ends here
