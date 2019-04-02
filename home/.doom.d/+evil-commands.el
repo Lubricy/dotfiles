c55dc113be08a44286ce539124fb29aee18efbe6
Lubricy Fibber <lubricy@gmail.com>
2019-04-02 15:26:32 +0800

    merged version

--- home/.doom.d/+evil-commands.el
+++ home/.doom.d/+evil-commands.el
@@ -1,6 +1,6 @@
-;;; +evil-commands.el  -*- lexical-binding: t; -*-
+;;; +evil-commands.el --- description -*- lexical-binding: t; -*-
 
-
+;; (ex! "q[uit]" 'delete-window)
 
 (provide '+evil-commands)
 ;;; +evil-commands.el ends here
