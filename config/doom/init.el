4c5600b6f2064640fb78ea8a9b3391f116e489ef
Lubricy Fibber <lubricy@gmail.com>
2024-01-03 17:30:39 +0800

    fix: Add nix to enabled modules
    feat: Set org-gtd-delegate-read-func to lubricy/find-people
    
    chore: Update git config
    
    chore: Ignore lock files in prepare-commit-msg script
    
    chore: Set default terminal to screen-256color

--- config/doom/init.el
+++ config/doom/init.el
@@ -154,7 +154,7 @@
        ;;lua               ; one-based indices? one-based indices
        markdown          ; writing docs for people to ignore
        ;;nim               ; python + lisp at the speed of c
-       ;;nix               ; I hereby declare "nix geht mehr!"
+       nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
        (org
         +jupyter
