4c56000cf2c9c4495e27a4138718759dd0be6ebf
Lubricy Fibber <lubricy@gmail.com>
2024-08-30 12:51:27 +0800

    feat(gptel): enhance pull request description generation in forge module

--- shared/doom/modules/private-tools/gptel/+forge.el
+++ shared/doom/modules/private-tools/gptel/+forge.el
@@ -1,8 +1,16 @@
 (defun +gptel-prepare-message ()
-  (let ((template (buffer-string))
+  (let ((template)
         (diff-str))
+    (goto-char (point-min))
+    (when (looking-at "#")
+      (kill-line)
+      (insert "# {[title of the pull request]}"))
+    (setq template (buffer-string))
     (erase-buffer)
-    (magit-fetch)
+    (magit-fetch-all nil)
+    (insert (format "\n\nCreating a pull request from %s to %s" forge--buffer-head-branch forge--buffer-base-branch))
+    (magit-git-insert "log" "--oneline" "--no-merges" forge--buffer-head-branch (format "^%s" forge--buffer-base-branch))
+    (insert "\n#+end_log\n")
     (insert "#+begin_diff\n")
     (magit-git-insert "diff" (format "%s...%s" forge--buffer-base-branch forge--buffer-head-branch))
     (insert "\n#+end_diff")
@@ -12,7 +20,10 @@
                     "#+begin_template\n%s\n#+end_template\n%s"
                     template
                     diff-str)
-      :system "generate a pull request description with the given template and diff."
+      :system (concat
+               "Generate a pull request description with the given template, git log and git diff. "
+               "DO NOT make up anything that does not exists in the log or diff. "
+               "If git log and git diff is empty, tell the user the PR is not needed in the description.")
       :stream t)))
 
 (setq forge-create-pullreq-hook
