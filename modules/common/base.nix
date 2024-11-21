4c5600f58320e4ed3d0e00a35aae9f195311c53e
Lubricy Fibber <lubricy@gmail.com>
2024-11-21 16:16:02 +0800

    chore: consolidate nix configuration

--- modules/common/base.nix
+++ modules/common/base.nix
@@ -48,32 +48,4 @@
 
   programs.zsh.enable = true;
   programs.nix-index-database.comma.enable = true;
-  nix.settings = {
-    # enable flakes globally
-    experimental-features = ["nix-command" "flakes"];
-
-    # given the users in this list the right to specify additional substituters via:
-    #    1. `nixConfig.substituers` in `flake.nix`
-    #    2. command line args `--options substituers http://xxx`
-    trusted-users = [config.vars.username];
-
-    # substituers that will be considered before the official ones(https://cache.nixos.org)
-    substituters = [
-      # cache mirror located in China
-      # status: https://mirror.sjtu.edu.cn/
-      # "https://mirror.sjtu.edu.cn/nix-channels/store"
-      # status: https://mirrors.ustc.edu.cn/status/
-      # "https://mirrors.ustc.edu.cn/nix-channels/store"
-
-      "https://nix-community.cachix.org"
-      # cuda-maintainer's cache server
-      "https://cuda-maintainers.cachix.org"
-    ];
-
-    trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
-    ];
-    builders-use-substitutes = true;
-  };
 }
--- modules/common/nix-core.nix
+++ modules/common/nix-core.nix
@@ -1,11 +1,12 @@
 {
   lib,
   nixpkgs,
+  config,
   ...
 }: {
   ###################################################################################
   #
-  #  Core configuration for nix-darwin
+  #  Core configuration
   #
   #  All the configuration options are documented here:
   #    https://daiderd.com/nix-darwin/manual/index.html#sec-options
@@ -17,14 +18,14 @@
   ###################################################################################
 
   # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.allowUnfree = lib.mkDefault true;
 
   # Disable auto-optimise-store because of this issue:
   #   https://github.com/NixOS/nix/issues/7273
   # "error: cannot link '/nix/store/.tmp-link-xxxxx-xxxxx' to '/nix/store/.links/xxxx': File exists"
-  nix.settings.auto-optimise-store = false;
+  nix.settings.auto-optimise-store = lib.mkDefault false;
 
-  nix.gc.automatic = false;
+  nix.gc.automatic = lib.mkDefault false;
 
   # make `nix run nixpkgs#nixpkgs` use the same nixpkgs as the one used by this flake.
   nix.registry.nixpkgs.flake = nixpkgs;
@@ -33,4 +34,40 @@
   # make `nix repl '<nixpkgs>'` use the same nixpkgs as the one used by this flake.
   # discard all the default paths, and only use the one from this flake.
   nix.nixPath = lib.mkForce ["/etc/nix/inputs"];
+
+  nix.settings = {
+    # enable flakes globally
+    experimental-features = ["nix-command" "flakes"];
+
+    # given the users in this list the right to specify additional substituters via:
+    #    1. `nixConfig.substituers` in `flake.nix`
+    #    2. command line args `--options substituers http://xxx`
+    trusted-users = [config.vars.username];
+
+    # substituers that will be considered before the official ones(https://cache.nixos.org)
+    substituters = [
+      # cache mirror located in China
+      # status: https://mirror.sjtu.edu.cn/
+      # "https://mirror.sjtu.edu.cn/nix-channels/store"
+      # status: https://mirrors.ustc.edu.cn/status/
+      # "https://mirrors.ustc.edu.cn/nix-channels/store"
+
+      "https://nix-community.cachix.org"
+      # cuda-maintainer's cache server
+      "https://cuda-maintainers.cachix.org"
+    ];
+
+    trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+    ];
+    builders-use-substitutes = true;
+
+    # prevent build inputs from being GC’d
+    keep-outputs = true;
+    # automatically run garbage collection whenever there is not enough space left
+    # free up to 1GiB whenever there is less than 100MiB left
+    min-free = 100 * 1024 * 1024;
+    max-free = 1024 * 1024 * 1024;
+  };
 }
