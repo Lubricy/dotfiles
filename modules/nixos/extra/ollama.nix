4c5600c7ac3edfe0e2e612796809714124b129c3
Lubricy Fibber <lubricy@gmail.com>
2024-12-02 15:00:26 +0800

    feat(nix-config): add support for ollama service in nixos configuration

--- modules/nixos/extra/ollama.nix
+++ modules/nixos/extra/ollama.nix
@@ -1,4 +1,14 @@
-{lib, ...}: {
+{
+  lib,
+  pkgs,
+  ...
+}: {
+  imports = [
+    "${pkgs.unstable}/nixos/modules/services/misc/ollama.nix"
+  ];
+  disabledModules = [
+    "services/misc/ollama.nix"
+  ];
   services.ollama = {
     enable = lib.mkDefault false;
     acceleration = "cuda";
