4c5600ae3e806fd10ba752b4389e9efbc7b09a0b
Lubricy Fibber <lubricy@gmail.com>
2025-04-10 08:53:45 +0800

    feat: update dependencies and remove unused code in Doom Emacs modules

--- shared/doom/modules/tweaks/org/+pretty.el
+++ shared/doom/modules/tweaks/org/+pretty.el
@@ -1,31 +1,43 @@
-(after! org-fancy-priorities
-  (setq org-priority-faces '((65 . error) (66 . success) (67 . warning)))
-  (setq org-fancy-priorities-list '("󰘃" "󰝔" "󰘄")))
+;; (after! org-fancy-priorities
+;;   (setq org-priority-faces '((65 . error) (66 . success) (67 . warning)))
+;;   (setq org-fancy-priorities-list '("󰘃" "󰝔" "󰘄")))
 
-(after! org
-  (plist-put! +ligatures-extra-symbols
-              :name    "❯"
-              :header  "»"
-              :begin  ""
-              :end  ""
-              :result ""
-              :return "󰌑 "
-              :yield "󱞹 "
-              )
-  (let* ((blocks
-          '("comment"
-            "example"
-            "notes"))
-         (ligatures
-          '(:name   "#+name:"
-            :header "#+header:"
-            :header "#+headers:"
-            :result "#+results:"))
-         (+ligatures (append ligatures
-                             (cl-loop for b in blocks
-                                      append `(:begin ,(concat "#+begin_" b)
-                                               :end ,(concat "#+end_" b))
-                                      ))))
-    (apply #'set-ligatures! 'org-mode
-           (cl-loop for (k v) on +ligatures by #'cddr
-                    append `(,k ,v ,k ,(upcase v))))))
+;; (after! org
+;;   (s)
+;;   (plist-put! +ligatures-extra-symbols
+;;               :name    "❯"
+;;               :header  "»"
+;;               :begin  ""
+;;               :end  ""
+;;               :result ""
+;;               :return "󰌑 "
+;;               :yield "󱞹 "
+;;               )
+;;   (let* ((blocks
+;;           '("comment"
+;;             "example"
+;;             "notes"))
+;;          (ligatures
+;;           '(:name   "#+name:"
+;;             :header "#+header:"
+;;             :header "#+headers:"
+;;             :result "#+results:"))
+;;          (+ligatures (append ligatures
+;;                              (cl-loop for b in blocks
+;;                                       append `(:begin ,(concat "#+begin_" b)
+;;                                                :end ,(concat "#+end_" b))
+;;                                       ))))
+;;     (apply #'set-ligatures! 'org-mode
+;;            (cl-loop for (k v) on +ligatures by #'cddr
+;;                     append `(,k ,v ,k ,(upcase v))))))
+
+(after! org-modern
+  (setq! org-modern-star 'replace
+         org-modern-keyword '(("results" . "")
+                              ("name" . "❯")
+                              ("header" . "»")
+                              (t . t))
+         org-modern-block-name '(("src"  "»" "«")
+                                 ("quote" "“" "”")
+                                 ("example" "" "")
+                                 (t . t))))
