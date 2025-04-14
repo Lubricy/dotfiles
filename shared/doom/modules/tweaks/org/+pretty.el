4c5600c0910b4988e5a88f3be3b8fd7646391961
Lubricy Fibber <lubricy@gmail.com>
2025-04-14 14:57:56 +0800

    feat: set org tag face

--- shared/doom/modules/tweaks/org/+pretty.el
+++ shared/doom/modules/tweaks/org/+pretty.el
@@ -1,43 +1,14 @@
-;; (after! org-fancy-priorities
-;;   (setq org-priority-faces '((65 . error) (66 . success) (67 . warning)))
-;;   (setq org-fancy-priorities-list '("󰘃" "󰝔" "󰘄")))
-
-;; (after! org
-;;   (s)
-;;   (plist-put! +ligatures-extra-symbols
-;;               :name    "❯"
-;;               :header  "»"
-;;               :begin  ""
-;;               :end  ""
-;;               :result ""
-;;               :return "󰌑 "
-;;               :yield "󱞹 "
-;;               )
-;;   (let* ((blocks
-;;           '("comment"
-;;             "example"
-;;             "notes"))
-;;          (ligatures
-;;           '(:name   "#+name:"
-;;             :header "#+header:"
-;;             :header "#+headers:"
-;;             :result "#+results:"))
-;;          (+ligatures (append ligatures
-;;                              (cl-loop for b in blocks
-;;                                       append `(:begin ,(concat "#+begin_" b)
-;;                                                :end ,(concat "#+end_" b))
-;;                                       ))))
-;;     (apply #'set-ligatures! 'org-mode
-;;            (cl-loop for (k v) on +ligatures by #'cddr
-;;                     append `(,k ,v ,k ,(upcase v))))))
-
 (after! org-modern
-  (setq! org-modern-star 'replace
-         org-modern-keyword '(("results" . "")
-                              ("name" . "❯")
-                              ("header" . "»")
-                              (t . t))
-         org-modern-block-name '(("src"  "»" "«")
-                                 ("quote" "“" "”")
-                                 ("example" "" "")
-                                 (t . t))))
+  (setq!
+   org-modern-star 'replace
+   org-modern-progress 'nil
+   org-modern-keyword '(("results" . "")
+                        ("name" . "❯")
+                        ("header" . "»")
+                        (t . t))
+   org-modern-block-name '(("src"  "»" "«")
+                           ("quote" "“" "”")
+                           ("example" "" "")
+                           (t . t))
+   org-modern-tag-faces '((t . (:background "#3f6f94"
+                                :foreground "white")))))
