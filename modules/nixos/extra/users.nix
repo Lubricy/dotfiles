4c56007bdba0656be9f6bc9f8ab0abdcc5d451c4
Lubricy Fibber <lubricy@gmail.com>
2025-04-15 08:40:30 +0800

    feat(nixos/users): add authorized keys to user configuration

--- modules/nixos/extra/users.nix
+++ modules/nixos/extra/users.nix
@@ -13,6 +13,8 @@ in {
       extraGroups = ["wheel" "networkmanager" "dialout" "input"]; # Enable ‘sudo’ for the user.
       home = "/home/${cfg.username}";
       shell = pkgs.zsh;
+      openssh.authorizedKeys.keyFiles = cfg.authorizedKeyFiles;
+      openssh.authorizedKeys.keys = cfg.authorizedKeys;
     };
   };
 }
