a778eda4e6c06e25dd5ded22936930448a45a3ba
Lubricy Fibber <lubricy@gmail.com>
2025-07-22 07:36:21 +0800

    feat(windows tweaks): optimize consult async throttle on Windows systems
    Add a configuration to adjust `consult-async-input-throttle` to 0.5 seconds on Windows systems. This change helps address performance issues that arise due to rapid process spawning, such as when using ripgrep for searching.

--- shared/doom/modules/tweaks/windows/config.el
+++ shared/doom/modules/tweaks/windows/config.el
@@ -1,3 +1,9 @@
 (when (file-exists-p "/proc/sys/fs/binfmt_misc/WSLInterop") ; HACK: inside WSL
   ;; HACK: fix clipboard coding system
   (setq! x-select-request-type '(text/plain\;charset=utf-8)))
+
+(when (featurep :system 'windows)
+  (after! consult
+    ;; HACK: Windows systems struggle with rapid process spawning,
+    ;; for example, when invoking ripgrep to search through a project.
+    (setq! consult-async-input-throttle 0.5)))
