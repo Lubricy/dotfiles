1fead32a39fc249f162fff29d9caa8a795b1a5ed
Lubricy Fibber <lubricy@gmail.com>
2026-01-21 07:31:02 +0800

    feat: add windows-specific eat package fork and configure multi-monitor setup with extended workspaces

--- shared/doom/modules/private-tools/eat/packages.el
+++ shared/doom/modules/private-tools/eat/packages.el
@@ -1,5 +1,8 @@
 (package! eat
-  :recipe (:host codeberg :repo "akib/emacs-eat"
+  :pin "windows-hack"
+  :recipe (:host codeberg
+           :repo "thearcticcat/emacs-eat"
+           :branch "windows-hack"
            :files ("*.el" ("term" "term/*.el") "*.texi"
                    "*.ti" ("terminfo/e" "terminfo/e/*")
                    ("terminfo/65" "terminfo/65/*")
